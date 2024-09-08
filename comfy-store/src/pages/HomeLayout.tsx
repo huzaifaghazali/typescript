22c8311280608ccd79538e82fb3f2eb882b8ca42
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-08 20:31:57 +0500

    Add Header component

--- comfy-store/src/pages/HomeLayout.tsx
+++ comfy-store/src/pages/HomeLayout.tsx
@@ -1,11 +1,16 @@
+import { Header } from '@/components';
 import { Outlet } from 'react-router-dom';
 
 function HomeLayout() {
-  return <>
-    <header>Header</header>
-    <nav>navbar</nav>
-    <Outlet />
-  </>;
+  return (
+    <>
+      <Header />
+      <nav>navbar</nav>
+      <div className='align-element py-20'>
+        <Outlet />
+      </div>
+    </>
+  );
 }
 
 export default HomeLayout;
