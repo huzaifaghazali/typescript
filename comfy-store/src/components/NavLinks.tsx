4b7db852a7e78449505348dc99544e63c59de96e
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-09 19:51:09 +0500

    Add NavLinks component

--- comfy-store/src/components/NavLinks.tsx
+++ comfy-store/src/components/NavLinks.tsx
@@ -1,5 +1,26 @@
+import { NavLink } from 'react-router-dom';
+import { links } from '@/utils';
+
 function NavLinks() {
-  return <div>NavLinks</div>;
+  return (
+    <div className='hidden lg:flex justify-center items-center gap-x-4'>
+      {links.map((link) => {
+        return (
+          <NavLink
+            key={link.label}
+            to={link.href}
+            className={({ isActive }) => {
+              return `capitalize font-light tracking-wide ${
+                isActive ? 'text-primary' : ''
+              }`;
+            }}
+          >
+            {link.label}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
 }
 
 export default NavLinks;
