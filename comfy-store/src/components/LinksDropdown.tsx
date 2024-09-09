a5102db115fca070dd08139f2d65487e521df3c1
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-09 19:11:49 +0500

    Add Links dropdown component

--- comfy-store/src/components/LinksDropdown.tsx
+++ comfy-store/src/components/LinksDropdown.tsx
@@ -1,5 +1,45 @@
+import { NavLink } from 'react-router-dom';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { Button } from './ui/button';
+import { AlignLeft } from 'lucide-react';
+import { links } from '@/utils';
+
 function LinksDropdown() {
-  return <div>LinksDropdown</div>;
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='lg:hidden'>
+        <Button variant='outline' size='icon'>
+          <AlignLeft />
+          <span className='sr-only'>Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className='w-52 lg:hidden '
+        align='start'
+        sideOffset={25}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.label}>
+              <NavLink
+                to={link.href}
+                className={({ isActive }) => {
+                  return `capitalize w-full ${isActive ? 'text-primary' : ''}`;
+                }}
+              >
+                {link.label}
+              </NavLink>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
 }
 
 export default LinksDropdown;
