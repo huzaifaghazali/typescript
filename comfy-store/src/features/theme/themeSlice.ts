30bc14a231bded8f81dad9ebc43dbc3851ce01a9
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-09 19:58:06 +0500

    Add utils function to apply theme

--- comfy-store/src/features/theme/themeSlice.ts
+++ comfy-store/src/features/theme/themeSlice.ts
@@ -1,8 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+export type Theme = 'dark' | 'light' | 'system';
+
 const initialState = {
   name: 'theme slice',
 };
+
 const themeSlice = createSlice({
   name: 'theme',
   initialState,
