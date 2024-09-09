6940d84b4d8a1388c8de7a5a760c193d81a540ce
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-09 21:23:44 +0500

    Add Cart Button component

--- comfy-store/src/components/CartButton.tsx
+++ comfy-store/src/components/CartButton.tsx
@@ -1,5 +1,24 @@
-function CartButton() {
-  return <div>CartButton</div>;
-}
+import { Link } from 'react-router-dom';
+import { ShoppingCart } from 'lucide-react';
+import { Button } from './ui/button';
 
+function CartButton() {
+  const numItemsInCart = 5;
+
+  return (
+    <Button
+      asChild
+      variant='outline'
+      size='icon'
+      className='flex justify-center items-center relative'
+    >
+      <Link to='/cart'>
+        <ShoppingCart />
+        <span className='absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs'>
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
+}
 export default CartButton;
