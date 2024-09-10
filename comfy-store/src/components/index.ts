2170e50e708a0346217f0f64d2e28bca885b0fb9
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-10 19:17:09 +0500

    Set up the landing page

--- comfy-store/src/components/index.ts
+++ comfy-store/src/components/index.ts
@@ -6,3 +6,8 @@ export { default as NavLinks } from './NavLinks';
 export { default as LinksDropdown } from './LinksDropdown';
 export { default as CartButton } from './CartButton';
 export { default as ErrorElement } from './ErrorElement';
+export { default as Hero } from './Hero';
+export { default as HeroCarousel } from './HeroCarousel';
+export { default as ProductGrid } from './ProductGrid';
+export { default as SectionTitle } from './SectionTitle';
+export { default as FeaturedProducts } from './FeaturedProducts';
--- comfy-store/src/pages/Landing.tsx
+++ comfy-store/src/pages/Landing.tsx
@@ -1,5 +1,10 @@
+import { FeaturedProducts, Hero } from "@/components";
+
 function Landing() {
-  return <h1 className='text-4xl'>Landing Page</h1>;
+  return <>
+    <Hero />
+    <FeaturedProducts />
+  </>;
 }
 
 export default Landing;
