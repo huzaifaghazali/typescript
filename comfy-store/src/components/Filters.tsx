2fdeeb27938857c5b0282f2acf1606a7c1c0d106
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-11 20:49:54 +0500

    Add form input component

--- comfy-store/src/components/Filters.tsx
+++ comfy-store/src/components/Filters.tsx
@@ -1,7 +1,7 @@
 import { Form, useLoaderData, Link } from 'react-router-dom';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import FormInput from './FormInput';
 import { Button } from './ui/button';
+import { ProductsResponseWithParams } from '@/utils';
 
 const Filters = () => {
   const { meta, params } = useLoaderData() as ProductsResponseWithParams;
@@ -9,10 +9,15 @@ const Filters = () => {
 
   return (
     <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
-      <div className='mb-2'>
-        <Label htmlFor='search'>Search Product</Label>
-        <Input id='search' name='search' type='text' defaultValue='' />
-      </div>
+      
+      {/* search */}
+      <FormInput
+        type='search'
+        label='search product'
+        name='search'
+        defaultValue={search}
+      />
+
       <Button type='submit' size='sm' className='self-end mb-2'>
         search
       </Button>
