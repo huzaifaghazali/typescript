1654233a10188bcb4eb1500d1845b4a4b83ad1be
huzaifaghazali <huzaifaghazali@gmail.com>
2024-09-17 12:44:00 +0500

    Add cart item component

--- comfy-store/src/components/CartItemsList.tsx
+++ comfy-store/src/components/CartItemsList.tsx
@@ -1,5 +1,32 @@
+import { useAppSelector } from '@/hooks';
+import { Card } from './ui/card';
+
+// import {
+//   FirstColumn,
+//   SecondColumn,
+//   ThirdColumn,
+//   FourthColumn,
+// } from './CartItemColumns';
+
 const CartItemsList = () => {
-  return <div>CartItemsList</div>;
+  const cartItems = useAppSelector((state) => state.cartState.cartItems);
+
+  return (
+    <div>
+      {cartItems.map((cartItem) => {
+        const { cartID, title, price, image, amount, company, productColor } =
+          cartItem;
+        return (
+          <Card
+            key={cartID}
+            className='flex flex-col gap-y-4 sm:flex-row flex-wrap p-6 mb-8'
+          >
+            Cart Item
+          </Card>
+        );
+      })}
+    </div>
+  );
 };
 
 export default CartItemsList;
